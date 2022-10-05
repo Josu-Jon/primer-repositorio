f075d6c9f3d92d0e87ce25cb0bb810525076e143
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2022-10-05 09:09:28 +0200

    Clase persona Con edad y nombre, con sus getterss y setters

--- proyecto_repositorio/src/proyecto_repositorio/Persona.java
+++ proyecto_repositorio/src/proyecto_repositorio/Persona.java
@@ -2,4 +2,25 @@ package proyecto_repositorio;
 
 public class Persona {
 	int edad;
+	String nombre;
+	
+	public Persona(int edad, String nombre) {
+		this.edad = edad;
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
