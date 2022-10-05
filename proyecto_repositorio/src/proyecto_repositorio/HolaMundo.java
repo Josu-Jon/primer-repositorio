944086bd80874b6bdcc4962258e383132deea30a
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2022-10-05 08:54:07 +0200

    Ampliacion del programa

--- proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
+++ proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
@@ -3,5 +3,6 @@ package proyecto_repositorio;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo ;-)");
+		System.out.println("Que tal? =0");
 	}
 }
