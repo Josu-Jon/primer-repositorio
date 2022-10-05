41d0f3679d7386c4f17385894205f959091f60f1
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2022-10-05 08:58:57 +0200

    3º Commit

--- proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
+++ proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
@@ -3,6 +3,6 @@ package proyecto_repositorio;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo ;-)");
-		System.out.println("Que tal? =0");
+		System.out.println("Que tal? = 0");
 	}
 }
