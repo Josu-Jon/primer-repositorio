712945d1ae0c0435a5c796f9b2d4abc050d87275
josujon.l.a <josujon.l.a@E105L11.opendeusto.es>
2022-10-07 12:18:41 +0200

    Comit de recordatorio

--- proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
+++ proyecto_repositorio/src/proyecto_repositorio/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo ;-)");
 		System.out.println("Que tal? = 0");
+		System.out.println("Bien :P");
 	}
 }
