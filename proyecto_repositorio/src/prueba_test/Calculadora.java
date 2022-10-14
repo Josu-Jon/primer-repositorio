13165bfb8226c34f9b13d96dda5ffcd71246943a
josujon.l.a <josujon.l.a@E105L11.opendeusto.es>
2022-10-14 12:45:30 +0200

    Test de Calculadora implementado

--- proyecto_repositorio/src/prueba_test/Calculadora.java
+++ proyecto_repositorio/src/prueba_test/Calculadora.java
@@ -3,7 +3,7 @@ package prueba_test;
 public class Calculadora {
 	public static int elevar(int base, int exp) {
 		int contador = 0;
-		int total = 0;
+		int total = 1;
 		while (contador < exp) {
 			total = total * base;
 			contador += 1;
--- proyecto_repositorio/test/prueba_test/CalculadoraTest.java
+++ proyecto_repositorio/test/prueba_test/CalculadoraTest.java
@@ -8,7 +8,7 @@ public class CalculadoraTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(9, Calculadora.elevar(3, 2));
 	}
 
 }
