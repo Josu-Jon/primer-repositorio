19b622f70d7acb1c86b8b21573e49fefb7030536
josujon.l.a <josujon.l.a@E105L11.opendeusto.es>
2022-10-21 13:49:06 +0200

    Primeras pruebas con BBDD

---
+++ proyecto_repositorio/src/prueba_BBDD/Base_Datos.java
@@ -0,0 +1,12 @@
+package prueba_BBDD;
+
+public class Base_Datos {
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se ha podido cargar el driver de la base de datos");
+		}
+		System.out.println("Programa terminado");
+	}
+}
